2cd6327c243bedb7585eba3ebe5c6f4ff7e87269
UncleAlexKun <raisin27@mail.ru>
2020-11-26 19:26:24 +0400

    Пример для домашнего задания

---
+++ HomeWork1/Lesson2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var daysInMarch = new int[31];
+
+			var rand = new Random();
+
+			Console.WriteLine("Март количество осадков:");
+			for (var i = 0; i < 31; i++)
+			{
+				daysInMarch[i] = rand.Next(0, 50);
+				Console.Write("{0}-{1}  ", i + 1, daysInMarch[i]);
+			}
+
+			var sumDaysRain = 0;
+			for (int i = 0; i < 31; i++)
+			{
+				if (daysInMarch[i] > 0)
+				{
+					sumDaysRain++;
+				}
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Количество дождливых дней: {0}", sumDaysRain);
+			Console.ReadKey();
+		}
+	}
+}
